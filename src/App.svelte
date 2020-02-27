a132443d69f7dab7bfd4b48feee9ac40603154dc
Edgar <Edgar@AnotherFoxGuy.com>
2020-02-27 10:19:58 +0100

    :tada: Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<script>
+    import {onMount} from 'svelte';
+
+    let m = {x: 0, y: 0};
+    let scz = {x: 0, y: 0};
+
+    function handleMousemove(event) {
+        m.x = event.clientX;
+        m.y = event.clientY;
+    }
+
+    onMount(async () => {
+        scz.x = document.getElementById("screen").offsetWidth;
+        scz.y = document.getElementById("screen").offsetHeight;
+    });
+
+    function addShape(event) {
+        m.x = event.clientX;
+        m.y = event.clientY;
+    }
+</script>
+
+<style>
+    div {
+        width: 100%;
+        height: 100%;
+    }
+
+    .second {
+        stroke: rgb(180, 0, 0);
+    }
+</style>
+
+<div id="screen" on:mousemove={handleMousemove} on:click={addShape}>
+    <svg viewBox='0 0 {scz.x} {scz.y}'>
+        <rect class="second" x="{m.x}" y="{m.y}" width="100" height="100"/>
+    </svg>
+</div>
---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+const path = require('path');
+
+const mode = process.env.NODE_ENV || 'development';
+const prod = mode === 'production';
+
+module.exports = {
+	entry: {
+		bundle: ['./src/main.js']
+	},
+	resolve: {
+		alias: {
+			svelte: path.resolve('node_modules', 'svelte')
+		},
+		extensions: ['.mjs', '.js', '.svelte'],
+		mainFields: ['svelte', 'browser', 'module', 'main']
+	},
+	output: {
+		path: __dirname + '/public',
+		filename: '[name].js',
+		chunkFilename: '[name].[id].js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.svelte$/,
+				use: {
+					loader: 'svelte-loader',
+					options: {
+						emitCss: true,
+						hotReload: true
+					}
+				}
+			},
+			{
+				test: /\.css$/,
+				use: [
+					/**
+					 * MiniCssExtractPlugin doesn't support HMR.
+					 * For developing, use 'style-loader' instead.
+					 * */
+					prod ? MiniCssExtractPlugin.loader : 'style-loader',
+					'css-loader'
+				]
+			}
+		]
+	},
+	mode,
+	devServer: {
+		inline: true,
+		hot: true
+	},
+	plugins: [
+		new webpack.ProgressPlugin(),
+		new webpack.HotModuleReplacementPlugin(),
+		new FriendlyErrorsWebpackPlugin(),
+		new HtmlWebpackPlugin({
+			filename: 'index.html',
+			template: './src/index.html',
+			inject: true
+		}),
+		new MiniCssExtractPlugin({
+			filename: '[name].css'
+		})
+	],
+	devtool: prod ? false: 'source-map'
+};
